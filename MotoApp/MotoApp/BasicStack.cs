1b041d80b3321bdc6df913f0672e8e3170842e93
Bartosz Kopacz <bbartekk93@gmail.com>
2025-03-30 21:35:10 +0200

    I -  WPROWADZENIE - Użycie stosu.

---
+++ MotoApp/MotoApp/BasicStack.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace MotoApp
+{
+    internal class BasicStack
+    {
+        private readonly double[] _items;
+        private int _currentIndex = -1;
+        public BasicStack() => _items = new double[10];
+        public int Count => _currentIndex + 1;
+        public void Push(double item) => _items[++_currentIndex] = item;
+        public double Pop() => _items[_currentIndex--];
+    }
+}
---
+++ MotoApp/MotoApp/Program.cs
@@ -0,0 +1,23 @@
+﻿// #1 WPROWADZENIE
+// Użycie stosu.
+
+using MotoApp;
+
+var stack = new BasicStack();
+stack.Push(4.5);
+stack.Push(43);
+stack.Push(333.6);
+
+double sum = 0.0;
+
+while (stack.Count > 0)
+{
+    double item = stack.Pop();
+    Console.WriteLine($"Item: {item}");
+    sum += item;
+}
+
+Console.WriteLine($"Sum: {sum}");
+
+// Po co nam parametry generyczne?
+// Generyczny Stos
