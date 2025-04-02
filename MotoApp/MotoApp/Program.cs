b0b1b2426c33b7f272514b96f9dfcd1c55205375
Bartosz Kopacz <bbartekk93@gmail.com>
2025-04-02 22:28:01 +0200

    II - SILA GENERYCZNOSCI W C# Jak napisać generyczne repozytorium

--- MotoApp/MotoApp/Program.cs
+++ MotoApp/MotoApp/Program.cs
@@ -1,15 +1,13 @@
 ﻿// II - SILA GENERYCZNOSCI W C#
-// Wprowadzenie - teoria
-// Odnalezienie zasobów w twoim projekcie - teoria
-// Dostep do zasobow z uzyciem repozytorium - teoria
-// Modele danych w twojej aplikacji
+// Jak napisać generyczne repozytorium
 
+using MotoApp.Entities;
 using MotoApp.Repositories;
 
-var employeeRepository = new EmployeeRepository();
+var employeeRepository = new GenericRepository<Employee>();
 employeeRepository.Add(new MotoApp.Entities.Employee { FirstName = "Adam" });
 employeeRepository.Add(new MotoApp.Entities.Employee { FirstName = "Piotr" });
 employeeRepository.Add(new MotoApp.Entities.Employee { FirstName = "Zuzia" });
 employeeRepository.Save();
 
-// Jak napisać generyczne repozytorium
+// Dziedziczenie typów generycznych
