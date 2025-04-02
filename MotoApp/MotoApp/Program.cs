0be1a95f7e87061373ce678075ac2fd5b08e0669
Bartosz Kopacz <bbartekk93@gmail.com>
2025-04-02 22:41:04 +0200

    II - SILA GENERYCZNOSCI W C# Wiele typow generycznych w jednej klasie

--- MotoApp/MotoApp/Program.cs
+++ MotoApp/MotoApp/Program.cs
@@ -1,13 +1,13 @@
 ﻿// II - SILA GENERYCZNOSCI W C#
-// Dziedziczenie typow generycznych
+// Wiele typow generycznych w jednej klasie
 
 using MotoApp.Entities;
 using MotoApp.Repositories;
 
-var employeeRepository = new GenericRepository<Employee>();
+var employeeRepository = new GenericRepository<Employee, int>();
 employeeRepository.Add(new MotoApp.Entities.Employee { FirstName = "Adam" });
 employeeRepository.Add(new MotoApp.Entities.Employee { FirstName = "Piotr" });
 employeeRepository.Add(new MotoApp.Entities.Employee { FirstName = "Zuzia" });
 employeeRepository.Save();
 
-// Wiele typow generycznych w jednej klasie
+// Ograniczenie typów generycznych
